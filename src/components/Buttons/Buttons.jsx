05c9cdc6a6803a88cd21a5ae7716ed4736548f8d
yeasir-coredevs <yeasir_hossain@coredevs.ltd>
2023-07-12 16:37:02 +0600

    structure

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -10,18 +10,18 @@ export const PrimaryButton = ({ title, className, icon }) => {
 
 export const SecondaryButton = ({ title, className, icon }) => {
     return (
-        <button className={`border py-2 px-5 bg-primary text-white rounded-50 text-sm ${className}`} >
-            {icon && icon}
-            {title && title}
+        <button className={`flex justify-center items-center space-x-2 border py-2 px-5 bg-primary text-white rounded-50 text-sm ${className}`} >
+            <img className="w-6 h-6" src={icon && icon} alt="" />
+            <p>{title && title}</p>
         </button>
     )
 }
 
 export const OutlineButton = ({ title, className, icon }) => {
     return (
-        <button className={`border py-2 px-5 border-secondary text-primary text-sm rounded-50 ${className}`} >
-            {icon && icon}
-            {title && title}
+        <button className={`flex justify-center items-center space-x-2 border py-2 px-5 border-secondary text-primary text-sm rounded-50 ${className}`} >
+            <img className="w-6 h-6" src={icon && icon} alt="" />
+            <p>{title && title}</p>
         </button>
     )
 }
